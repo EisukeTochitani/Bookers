b4e906652199cf27e818dbefb88c7152b0932f4e
EisukeTochitani <kop.michael1958@gmail.com>
2021-02-13 14:08:55 +0000

    [finish]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,18 +7,42 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+  	@book = Book.find(params[:id])
   end
 
-  def cleate
+  def create
+    @books = Book.all
     @book = Book.new(book_params)
-    if @book.save
-      flash[:notice] = "successfully"
-      redirect_to book_path(@book)
-    end
+      if
+        @book.save
+        flash[:notice] = "successfully"
+        redirect_to book_path(@book)
+      else
+        @books = Book.all
+        render :index
+      end
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+    	@book = Book.find(params[:id])
+  	 if
+  	   @book.update(book_params)
+  	   flash[:notice] = "successfully"
+  	   redirect_to book_path(@book)
+     else
+       render :edit
+    end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	flash[:notice] = "successfully"
+  	redirect_to books_path
   end
 
   private
